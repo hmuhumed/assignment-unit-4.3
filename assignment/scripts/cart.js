07dc5e664e995293ec0f8cca864133badddc8346
Hamza Muhumed <hamzamuhumed2107@gmail.com>
2023-04-23 16:29:29 -0600

    Made a cart full of items using arrays and we used funtions and conditionals to list all the items in our array which we called basket.

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,36 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+
+let basket = [] ; 
+console.log('Made a basket array.');
+function addItems(item){
+    console.log('We picked up:' , item);
+    basket.push(item);
+    return basket;
+};
+
+addItems('Milk');
+addItems('Rice');
+addItems('Beef');
+addItems('Chicken');
+addItems('Potato');
+
+function listItem(){
+    for (i = 0; i<basket.length; i++){
+        console.log('What is in our basket? ' , basket[i]);
+        console.log(basket[0]);
+        console.log(basket[1]);
+        console.log(basket[2]);
+        console.log(basket[3]);
+        console.log(basket[4]);
+    };
+    
+};
+
+function empty() {
+    basket.length = 0;
+    console.log('All items have been removed:' , basket);
+};
+
+empty();
